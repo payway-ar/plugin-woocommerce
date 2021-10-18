a2f81c9a95687e1590d9ed5aaf29857124af051d
juangpereziurco <75809737+juangpereziurco@users.noreply.github.com>
2021-10-18 15:06:32 -0300

    Develop (#19)
    * Fix/change transation id to string (#9)
    
    * Fix production keys
    
    * fix public key
    
    * add installer zip
    
    * delete old zip
    
    * add files cybersource
    
    * fix cybersource
    
    * change transsaction to
     string
    
    * fix value
    
    * test CS
    
    * fix product array to cybersource
    
    * add console.log to process payment
    
    * fix array products
    
    * Change method use CS
    
    * fix array products
    
    * fix array products
    
    * add log transactions
    
    Co-authored-by: juangpereziurco <75809737+juangpereziurco@users.noreply.github.com>
    Co-authored-by: Juan G. Perez <juangustavo@iurco.com>
    Co-authored-by: Manuel Strajman <manuel.strajman@redb.ee>
    
    * Rc3.1 (#7)
    
    * Fix production keys
    
    * fix public key
    
    * add installer zip
    
    * delete old zip
    
    * add files cybersource
    
    * fix cybersource
    
    * add promotions
    
    * add modifications readme file
    
    Co-authored-by: Manuel Strajman <manuel.strajman@redb.ee>
    Co-authored-by: Marcelo Mansilla <83774187+mmansilla-prisma@users.noreply.github.com>
    
    * Feature/fix merge (#14)
    
    * Fix production keys
    
    * fix public key
    
    * add installer zip
    
    * delete old zip
    
    * add files cybersource
    
    * fix cybersource
    
    * Fix/change transation id to string (#9) (#11)
    
    * Fix production keys
    
    * fix public key
    
    * add installer zip
    
    * delete old zip
    
    * add files cybersource
    
    * fix cybersource
    
    * change transsaction to
     string
    
    * fix value
    
    * test CS
    
    * fix product array to cybersource
    
    * add console.log to process payment
    
    * fix array products
    
    * Change method use CS
    
    * fix array products
    
    * fix array products
    
    * add log transactions
    
    Co-authored-by: juangpereziurco <75809737+juangpereziurco@users.noreply.github.com>
    Co-authored-by: Juan G. Perez <juangustavo@iurco.com>
    Co-authored-by: Manuel Strajman <manuel.strajman@redb.ee>
    
    Co-authored-by: juangpereziurco <75809737+juangpereziurco@users.noreply.github.com>
    Co-authored-by: Juan G. Perez <juangustavo@iurco.com>
    Co-authored-by: Manuel Strajman <manuel.strajman@redb.ee>
    Co-authored-by: Mariano Fernandez <marfernandez@serv.externos.prismamp.com>
    
    * change transsaction to string (#8)
    
    * change transsaction to
     string
    
    * fix value
    
    * test CS
    
    * fix product array to cybersource
    
    * add console.log to process payment
    
    * fix array products
    
    * Change method use CS
    
    * fix array products
    
    * fix array products
    
    * add log transactions
    
    Co-authored-by: juangpereziurco <75809737+juangpereziurco@users.noreply.github.com>
    Co-authored-by: Juan G. Perez <juangustavo@iurco.com>
    Co-authored-by: Manuel Strajman <manuel.strajman@redb.ee>
    Co-authored-by: MnoFernandez <76661036+MnoFernandez@users.noreply.github.com>
    Co-authored-by: Mariano Fernandez <marfernandez@serv.externos.prismamp.com>
    
    * Revert "Feature/fix merge (#14)" (#17)
    
    This reverts commit 734c67437ede8ae52dd26ea2487200d727ffdf2d.
    
    Co-authored-by: MnoFernandez <76661036+MnoFernandez@users.noreply.github.com>
    Co-authored-by: Manuel Strajman <manuel.strajman@redb.ee>
    Co-authored-by: Marcelo Mansilla <83774187+mmansilla-prisma@users.noreply.github.com>
    Co-authored-by: Mariano Fernandez <marfernandez@serv.externos.prismamp.com>

--- WC-gateway-decidir/decidir/vendor/decidir2/php-sdk/Decidir/lib/Connector.php
+++ WC-gateway-decidir/decidir/vendor/decidir2/php-sdk/Decidir/lib/Connector.php
@@ -1,30 +1,78 @@
 <?php
+
 namespace Decidir;
+
+
 
 define('DECIDIR_VERSION','1.5.0');
 
-class Connector {
-	private $header_http = array();
-	private $mode = NULL;
-	private $healthCheck = NULL;
-	private $paymentInstance = NULL;
-	private $tokenInstance = NULL;
-	private $developer = NULL;
-    private $grouper = NULL;
-    private $service = NULL;
 
-	public function __construct($header_http_array, $mode, $developer = "", $grouper = "", $service = "SDK-PHP"){
+
+class Connector {
+
+	private $header_http = array();
+
+	private $mode = NULL;
+
+	private $healthCheck = NULL;
+
+	private $paymentInstance = NULL;
+
+	private $tokenInstance = NULL;
+
+
+
+	public function __construct($header_http_array, $mode){
+
+
 
 		$this->mode = $mode;
+
 		$this->header_http = $header_http_array;
-		$this->developer = $developer;
-        $this->grouper = $grouper;
-        $this->service = $service;
+
+
 
 		$this->healthCheck = new \Decidir\HealthCheck($this->header_http, $this->mode);
-		$this->paymentInstance = new \Decidir\Payment($this->header_http, $this->mode, $this->developer, $this->grouper, $this->service);
+
+		$this->paymentInstance = new \Decidir\Payment($this->header_http, $this->mode);
+
 		$this->tokenInstance = new \Decidir\Tokenization($this->header_http, $this->mode);
+
 	}
+
+
+
+	public function healthcheck(){
+
+		return $this->healthCheck;
+
+	}
+
+
+
+	public function payment(){
+
+		return $this->paymentInstance;
+
+	}
+
+
+
+	public function cardToken(){
+
+		return $this->tokenInstance;
+
+	}
+
+
+
+	public function paymentToken(){
+
+		return $this->tokenInstance;
+
+	}
+
+}
 
 	public function healthcheck(){
 		return $this->healthCheck;
@@ -42,3 +90,4 @@ class Connector {
 		return $this->tokenInstance;
 	}
 }
+
